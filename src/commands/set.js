5e94e63682fb31e56a95d773306efd8647c01450
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-08-09 15:17:48 +0900

    Forgot to push set.js
    Oops

---
+++ src/commands/set.js
@@ -0,0 +1,84 @@
+const { MessageEmbed } = require("discord.js");
+
+const set = async (message, args) => {
+	const role = message.guild.members.cache.get(client.user.id).roles.cache.filter(r => r.managed);
+	
+	if (args[0] === "status") {
+		const mes = await message.channel.send("設定しています...");
+		
+		let embed = new MessageEmbed()
+			.setColor("GRAY")
+			.setTitle("ステータス")
+			.setDescription("データを取得中です...")
+			.setTimestamp(new Date);
+			
+		await message.channel.overwritePermissions([
+			{ 
+				id: role.first(),
+				allow: "SEND_MESSAGES",
+			},
+			{ 
+				id: message.guild.roles.everyone,
+				deny: "SEND_MESSAGES", 
+			},
+		]);
+		
+		mes.edit("", embed).then(() => message.delete());
+		
+		client.database.channelUpdate("status", mes.channel.id, mes.id);
+		
+		if (client.socketManager.isConnected()) {
+			client.socketManager.sendToServer({ event: "status" });
+		} else {
+			embed = new MessageEmbed()
+				.setColor("BLACK")
+				.setTitle("ステータス")
+				.setDescription("オフライン")
+				.setTimestamp(new Date);
+				
+			mes.edit(embed);
+		}
+	} else if (args[0] === "chat") {
+		client.database.channelUpdate("chat", message.channel.id);
+		
+		message.channel
+			.createWebhook("Minecraft", { avatar: "https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/iso-grassblock.png" })
+			.then(async () => {
+				await message.channel.overwritePermissions([
+					{ 
+						id: role.first(),
+						allow: "SEND_MESSAGES",
+					},
+					{ 
+						id: message.guild.roles.everyone,
+						deny: "SEND_MESSAGES", 
+					},
+				]);
+				
+				client.socketManager.fetchWebhook();
+				
+				message.channel.send("設定しました").then((m) => m.delete({ timeout: 10000 }).then(() => message.delete()));
+				
+				if (!client.socketManager.isConnected) client.disableChat();
+			})
+			.catch(async (e) => {
+				console.log(e);
+				message.channel.send("設定に失敗しました").then((m) => m.delete({ timeout: 10000 }).then(() => message.delete()));
+			});
+	} else {
+		message.channel.send("不明な引数です").then((m) => m.delete({ timeout: 10000 }).then(() => message.delete()));
+	}
+};
+
+module.exports = {
+	name: "set",
+	aliases: ["s"],
+	description: "チャンネル設定",
+	usage: "set <status | chat>",
+	owner: true,
+	args: {
+		need: true,
+		max: 1,
+	},
+	run: set,
+};
