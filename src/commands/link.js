d72621675702d39773b64b91a65a5962502c7af6
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-08-12 21:21:48 +0900

    Remove link and any other message when in chat channel

--- src/commands/link.js
+++ src/commands/link.js
@@ -1,14 +1,21 @@
 const link = async (message, args) => {
-	if (!client.socketManager.isConnected()) return message.channel.send("現在、サーバーが起動していないかオフラインのため、このコマンドを実行することができません");
+	let mes;
 	
-	if (!args.length) {
+	if (!client.socketManager.isConnected()) {
+		mes = message.channel.send("現在、サーバーが起動していないかオフラインのため、このコマンドを実行することができません");
+	} else if (!args.length) {
 		const code = client.linkManager.new(message.author.id, null);
-		message.channel.send(`今から1分以内に、マイクラに \`/link ${code}\` を送信してください`);
+		mes = message.channel.send(`今から1分以内に、マイクラに \`/link ${code}\` を送信してください`);
 	} else {
 		client.linkManager.check(args[0], message.author.id, null)
-			.then(result => message.channel.send(result))
-			.catch(err => message.channel.send(err));
-		
+			.then(result => mes = message.channel.send(result))
+			.catch(err => mes = message.channel.send(err));
+	}
+	
+	const info = client.database.getFromUSE("chat");
+	if (info && message.channel.id === info.channelID) {
+		message.delete({ timeout: 3000 });
+		if (mes) mes.delete({ timeout: 3000 });
 	}
 };
 
