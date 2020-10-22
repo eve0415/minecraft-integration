488e97d22f355394042f65ebdbc59e4868eb1391
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-10-22 22:19:46 +0900

    Will re-implement later

--- src/events/Discord/message.js
+++ src/events/Discord/message.js
@@ -2,14 +2,9 @@ module.exports = async (instance, message) => {
   if (message.author === instance.client.user) return;
   if (message.author.bot) return;
   
-  const info = instance.database.getFromType('chat');
-  if (message.channel.id !== info?.channelID && message.content.startsWith('/')) return instance.socketManager.commandController(message);
-  if (message.channel.id === info?.channelID && !message.content.startsWith(`${instance.config.prefix}link`)) return instance.socketManager.sendFromDiscord(message);
-  
   const result = instance.parser.parse(message.content);
   
   const cmd = instance.commands.get(result.command);
-  
   if (!cmd) return;
   
   // Identify
