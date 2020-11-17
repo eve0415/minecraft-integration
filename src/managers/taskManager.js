9686eb100cfdb947b9e348d082c865f72c6e490a
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-11-18 00:00:32 +0900

    Wait for the bot to be ready to server

--- src/managers/taskManager.js
+++ src/managers/taskManager.js
@@ -13,73 +13,74 @@ module.exports = class TaskManager {
     this.logChannels    = new Array;
   }
   
-  reloadCache() {
+  async reloadCache() {
     if (!this.client.readyAt) throw new Error('Bot is not ready');
-    this.cacheStatusMessage();
-    this.cacheWebhooksForChat();
-    this.cacheWebhooksForLog();
+    await Promise.all([this.cacheStatusMessage(), this.cacheWebhooksForChat(), this.cacheWebhooksForLog()]);
+    this.ready = true;
   }
   
   cacheStatusMessage() {
     this.instance.logger.info('Trying to cache necessarily message');
     
     const cache = this.database.getStatusMesCache();
-    if (!cache.length) return this.instance.logger.info('No message to cache!');
-    
-    for (const c of cache) {
-      this.client.channels.cache.get(c.channelID).messages.fetch(c.messageID)
-        .then(mes => {
-          const data = embedParse(mes);
-          if (data.Page) this.instance.reactionController.init(mes);
-          this.statusMessage.push(mes);
-        })
-        // Probably user have deleted this channel / message or have no permission to fetch anymore :(
-        // Remove from cache database
-        .catch(() => this.database.removeStatusMessage(c.messageID));
+    if (!cache.length) {
+      this.instance.logger.info('No message to cache!');
+    } else {
+      for (const c of cache) {
+        this.client.channels.cache.get(c.channelID).messages.fetch(c.messageID)
+          .then(mes => {
+            const data = embedParse(mes);
+            if (data.Page) this.instance.reactionController.init(mes);
+            this.statusMessage.push(mes);
+          })
+          .catch(() => this.database.removeStatusMessage(c.messageID));
+      }
+      
+      this.instance.logger.info('Succesfully cached messages.');
+      this.refreshStatus();
     }
-    
-    this.instance.logger.info('Succesfully cached messages.');
-    this.refreshStatus();
+    return Promise.resolve;
   }
   
   cacheWebhooksForLog() {
     this.instance.logger.info('Trying to cache all webhooks for logging');
     
     const cache = this.database.getAllChannelLog();
-    if (!cache.length) return this.instance.logger.info('No webhooks to cache for logging!');
-    
-    for (const c of cache) {
-      this.client.channels.cache.get(c.channelID).fetchWebhooks()
-        .then(webhooks => {
-          const webhook = webhooks.filter(w => w.owner === this.instance.client.user).first();
-          this.logChannels.push(new webhookManager(c.serverID, webhook));
-        })
-        // Probably user have deleted this channel / webhooks or have no permission to fetch anymore :(
-        // Remove from cache database
-        .catch(() => this.database.removeChannelCache(c.channelID));
+    if (!cache.length) {
+      this.instance.logger.info('No webhooks to cache for logging!');
+    } else {
+      for (const c of cache) {
+        this.client.channels.cache.get(c.channelID).fetchWebhooks()
+          .then(webhooks => {
+            const webhook = webhooks.filter(w => w.owner === this.instance.client.user).first();
+            this.logChannels.push(new webhookManager(c.serverID, webhook));
+          })
+          .catch(() => this.database.removeChannelCache(c.channelID));
+      }
+      this.instance.logger.info('Succesfully cached webhook to send logs.');
     }
-    this.instance.logger.info('Succesfully cached webhook to send logs.');
+    return Promise.resolve;
   }
   
   cacheWebhooksForChat() {
     this.instance.logger.info('Trying to cache all webhook for chatting');
     
     const cache = this.database.getAllChannelCache();
-    if (!cache.length) return this.instance.logger.info('No webhooks to cache for chatting!');
-    
-    for (const c of cache) {
-      this.client.channels.cache.get(c.channelID).fetchWebhooks()
-        .then(webhooks => {
-          const webhook = webhooks.filter(w => w.owner === this.instance.client.user).first();
-          this.webhooks.push(new webhookManager(c.serverID, webhook));
-        })
-        // Probably user have deleted this channel / webhooks or have no permission to fetch anymore :(
-        // Remove from cache database
-        .catch(() => this.database.removeChannelCache(c.channelID));
+    if (!cache.length) {
+      this.instance.logger.info('No webhooks to cache for chatting!');
+    } else {
+      for (const c of cache) {
+        this.client.channels.cache.get(c.channelID).fetchWebhooks()
+          .then(webhooks => {
+            const webhook = webhooks.filter(w => w.owner === this.instance.client.user).first();
+            this.webhooks.push(new webhookManager(c.serverID, webhook));
+          })
+          .catch(() => this.database.removeChannelCache(c.channelID));
+      }
+      
+      this.instance.logger.info('Successfully cached webhook for chatting.');
     }
-    
-    this.instance.logger.info('Successfully cached webhook for chatting.');
-    this.ready = true;
+    return Promise.resolve;
   }
   
   refreshStatus() {
@@ -142,6 +143,8 @@ module.exports = class TaskManager {
   }
   
   sendLog(id, embed) {
+    if (!this.ready) return;
+    
     const filtered = this.logChannels.filter(data => data.id === id || data.id === 0);
     filtered.forEach(data => {
       data.sendLog(embed);
