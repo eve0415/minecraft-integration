67b99c661403018b2a4bbddee69f95fcb4c0533f
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-12-25 00:48:24 +0900

    Create config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,29 @@
+import { logger } from '../logger';
+
+export type ConfigInterface = {
+  prefix: string;
+  owner: string;
+  port: number;
+};
+
+class Config implements ConfigInterface {
+  public readonly prefix: string;
+  public readonly owner: string;
+  public readonly port: number;
+
+  public constructor() {
+      this.checkEnvironment();
+      this.prefix = process.env.botPrefix ?? '/';
+      this.owner = process.env.owner ?? '';
+      this.port = Number(process.env.port) ?? 0;
+  }
+
+  private checkEnvironment() {
+      if (!process.env.DISCORD_TOKEN) return new Error('Please set DISOCRD_TOKEN in your .env file');
+      if (!process.env.owner) return new Error('No owner ID found! Please set owner in your /env file');
+      if (!process.env.port) return new Error('No port number found! Please set port in your .env file');
+      if (!process.env.prefix) logger.warn('No prefix was set! Using the default / for prefix');
+  }
+}
+
+export default new Config;
