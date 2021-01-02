f51e1ae7a3bad2df98c9317ba1c9cbde0ac4d3fe
eve0415 <40632436+eve0415@users.noreply.github.com>
2021-01-02 13:37:00 +0900

    Needs partials for bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,15 +1,15 @@
-import { Client as DiscordBot } from 'discord.js';
+import { Client } from 'discord.js';
 import { logger } from '.';
 import { CommandManager, DJSEventManager } from './Managers';
 import { ConfigInterface } from './config';
 
-export class DJSClient extends DiscordBot {
-  public readonly config: ConfigInterface;
+export class DJSClient extends Client {
+  protected readonly config: ConfigInterface;
   private readonly commands: CommandManager;
   private readonly events: DJSEventManager;
 
   public constructor(config: ConfigInterface) {
-      super({ restTimeOffset: 100 });
+      super({ restTimeOffset: 5, partials: ['GUILD_MEMBER'] });
       this.config = config;
       this.commands = new CommandManager(this);
       this.events = new DJSEventManager(this);
