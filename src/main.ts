a17619b808c9106d661e48b11472b5e84316f874
eve0415 <40632436+eve0415@users.noreply.github.com>
2021-01-09 00:09:42 +0900

    That wasn't suppose to happen

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ export class Instance {
     public readonly config: ConfigInterface;
     public readonly bot: DJSClient;
     private readonly ws: websocketClient;
-    public readonly MinecraftStatusManager: MinecraftStatusManager;
+    public readonly statusManager: MinecraftStatusManager;
     public readonly logManager: MinecraftLogManager;
     public readonly chatManager: MinecraftChatManager;
 
@@ -18,7 +18,7 @@ export class Instance {
 
         this.bot = new DJSClient(this);
         this.ws = new websocketClient(this);
-        this.MinecraftStatusManager = new MinecraftStatusManager(this);
+        this.statusManager = new MinecraftStatusManager(this);
         this.logManager = new MinecraftLogManager(this);
         this.chatManager = new MinecraftChatManager(this);
 
@@ -57,7 +57,7 @@ export class Instance {
     // or else it won't work.
     private async postInit() {
         logger.info('Post initialization started!');
-        await this.MinecraftStatusManager.init();
+        await this.statusManager.init();
         await this.logManager.init();
         await this.chatManager.init();
         logger.info('Initialize complete...');
