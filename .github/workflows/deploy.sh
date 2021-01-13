f804011f5b38cc07d5012e0600dc41ac2e7e267f
eve0415 <40632436+eve0415@users.noreply.github.com>
2021-01-13 20:14:30 +0900

    Typo

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -14,7 +14,7 @@ do
     if [ "$RUNNING" != "$LATEST" ];then
         echo "upgrading $IMAGE"
         docker stop $NAME
-        docker run -v $PWD/data:/app/data/ --env-file .env -p 25500:25500 --name MI -itd --rm eve0415/minecraft-integration
+        docker run -v $PWD/data:/app/data/ --env-file .env -p 25500:25500 --name MI -itd --rm $IMAGE
     else
         echo "$IMAGE is up to date"
     fi
