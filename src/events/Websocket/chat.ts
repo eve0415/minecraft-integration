2e36d6186dda5fdf6557c1f2b5ffc002c4759402
eve0415 <40632436+eve0415@users.noreply.github.com>
2021-05-01 23:55:43 +0900

    Fix error on replacing undefined

--- src/events/Websocket/chat.ts
+++ src/events/Websocket/chat.ts
@@ -7,7 +7,7 @@ export default class extends WebsocketEvent {
     }
 
     public run(data: ChatData): void {
-        data.message = this.client.instance.localizations?.strings.translate[data.message as keyof MinecraftKey['translate']].replace('%s', data.name) ?? data.message;
+        data.message = this.client.instance.localizations?.strings.translate[data.message as keyof MinecraftKey['translate']]?.replace('%s', data.name) ?? data.message;
 
         this.client.instance.chatManager.sendWebhook(data);
     }
